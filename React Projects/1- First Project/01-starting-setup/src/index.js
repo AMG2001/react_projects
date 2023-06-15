a4b2747bc5cfd6a29c189478531b3fac639ac0f8
Mohamad amgad elsaid <mohamadamgad09@gmail.com>
2023-06-15 14:37:26 +0300

    before adding css

---
+++ React Projects/1- First Project/01-starting-setup/src/index.js
@@ -0,0 +1,8 @@
+// here we import from library called react-dom , you can find it in package.json file .
+import ReactDOM from 'react-dom/client';
+
+import './index.css';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
